e48d98a3f5d6f9423bbbeb09ad5bd8590112beb2
Claudio Melgaco <claudio19a@hotmail.com>
2022-05-24 16:32:52 -0300

    Teste 9

--- src/challenges.js
+++ src/challenges.js
@@ -79,10 +79,20 @@ function fizzBuzz() {
 }
 
 // Desafio 9
-function encode() {
+function encode(frase) {
+  return frase.replace(/a/g , '1')
+  .replace(/e/g , '2')
+  .replace(/i/g , '3')
+  .replace(/o/g , '4')
+  .replace(/u/g , '5'); 
   // seu código aqui
 }
-function decode() {
+function decode(frase) {
+  return frase.replace(/1/g , 'a')
+  .replace(/2/g , 'e')
+  .replace(/3/g , 'i')
+  .replace(/4/g , 'o')
+  .replace(/5/g , 'u'); 
   // seu código aqui
 }
 
